6b30f14579b5e96d5aa47f0d44f09132a383e01c
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2022-11-07 15:25:56 +0200

    More javascript

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Hello")
+
+
+function write(){
+    console.log("functionen kördes")
+}
+
+write()
+
+let namn = "hello"
+
+
+function buttonHandler(){
+    alert("du klickade här")
+}
+
+function secondButton(){
+    const namn = prompt("Vad heter du?")
+
+    alert("Hello" + namn)
+
+
+}
+const namn1 = namnInput.value
+function thirdButton(){
+    const namnInput = document.querySelector("#namn").value
+    document.querySelector("#hello-output").innerText = `Morjens ${namnInput}!`
+}
+
